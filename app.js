9bcb1c109b6f9cc5cbb592ab3d7edbdbea74dbdb
unknown <renetdevp@gmail.com>
2025-04-22 17:32:55 +0900

    ADD: Create Topbar/Navbar as ejs template

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const path = require('node:path');
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static(path.join(__dirname, 'static')));
+
+app.get('/', (req, res) => {
+    res.render('main');
+});
+
+app.listen(3000, () => {
+    console.log('hos server opened');
+});
