6f46924cddccfac26db884ea8a70b208727e6096
SooowanLee <lsy9406@gmail.com>
2025-04-24 23:08:48 +0900

    REMOVE: mainPageCard로 전송하던 videoList데이터 삭제

--- app.js
+++ app.js
@@ -11,21 +11,8 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(path.join(__dirname, 'static')));
 app.use(cors());
 
-const contentNumbers = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-
-const videoList = Array.from({ length: 16 }, () => ({
-  videoSrc: '/videos/sample1.mp4',
-  poster: ` /public/img/videoPage/rVideo${contentNumbers(1, 8)}.png`,
-  userProfile: `/public/img/navBar/subUserIcon${contentNumbers(1, 7)}.svg`,
-  description: 'Lorem ipsum dolor sit amet, consecte adipiscing elit.',
-  username: 'James Gouse',
-  views: '15K views 1 week ago',
-  runningTime: '23:45',
-}));
-
 app.get("/", (req, res) => {
-  res.render("main", { videoList});
+  res.render("main");
 });
 
 app.get('/video', (req, res) => {
