bc7881905d990dc9039958912bef6f62fabfb9bb
renetdevp <renetdevp@gmail.com>
2025-04-24 12:15:17 +0900

    MOD: Get video metadata from API Server

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const path = require('node:path');
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.render('main');
