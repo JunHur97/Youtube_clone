51e7a55378ea9952552950d9440512ee83503f54
renet <renetdevp@gmail.com>
2025-04-25 11:25:31 +0900

    MOD: Move videoPage files to views/videos/ (#32)

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+
 const path = require('node:path');
 
 app.set('view engine', 'ejs');
-
-// 정적 파일 경로 설정
+// https://stackoverflow.com/questions/25858431/when-using-express-with-nodejs-how-does-view-cache-work
+// view cache는 dev 환경에선 설정되지 않으니 굳이 꺼줄 필요도 없음
+// app.set('view cache', false);
 app.use('/public', express.static(path.join(__dirname, 'static')));
-
-// CORS 허용
 app.use(cors());
 
-// 메인 페이지
 app.get("/", (req, res) => {
   res.render("main");
 });
 
-// ✅ 수정: videoPage 경로를 yeeunkim7 폴더 내부로!
 app.get('/video', (req, res) => {
-  res.render('yeeunkim7/videoPage');
     res.render('videos/videoPage');
 });
 
-// 채널 페이지
 app.get('/channel', (req, res) => {
-  res.render('channelpage');
+    res.render('channelpage');
 });
 
-// 서버 시작
 app.listen(3000, () => {
-  console.log('hos server opened');
+    console.log('hos server opened');
 });
