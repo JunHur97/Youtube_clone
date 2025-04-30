7a772259afc169d0db1cd39a424531ef9f32db2a
renet <renetdevp@gmail.com>
2025-04-30 10:13:04 +0900

    Set channel name in subscription section of navBar (#70)
    * MOD: Modify border-radius of channel image in navBar
    
    * MOD: Set channel name in subscription section of navBar

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -1,15 +1,19 @@
-function insertNavbarSub(chId){
+async function insertNavbarSub(chId){
+    const { channel_name } = await getChannel(chId);
+
     const hos = `
-    <div class="navItem" chId=${chId}>
-        <img src="https://storage.googleapis.com/youtube-clone-video/channel_profile_${chId}.png" />
-        <p>Skylar Dias</p>
-    </div>`;
+    <a href="/channels?ch_id=${chId}">
+        <div class="navItem" chId=${chId}>
+            <img src="https://storage.googleapis.com/youtube-clone-video/channel_profile_${chId}.png" />
+            <p>${channel_name}</p>
+        </div>
+    </a>`;
 
     $('section.subscription')[0].insertAdjacentHTML('beforeend', hos);
 }
 
 function removeNavbarSub(chId){
-    const sub = $(`section.subscription > .navItem[chid=${chId}]`)[0];
+    const sub = $(`section.subscription .navItem[chid=${chId}]`)[0];
 
     sub.remove();
 }
