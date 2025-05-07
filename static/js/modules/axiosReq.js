37799225dc30b177f80b14b35b59b9b47f7af57c
renet <renetdevp@gmail.com>
2025-05-07 09:59:02 +0900

    Add Code description, Change Indent, Delete unused package (#74)
    * MOD: Change indent, Add JS function params description
    
    * MOD: Delete unused package

--- static/js/modules/axiosReq.js
+++ static/js/modules/axiosReq.js
@@ -1,50 +1,67 @@
 import { getDataFromCache, insertDataInCache } from '../localCache.js';
 
+const baseUrl = 'http://techfree-oreumi-api.kro.kr:5000';
+
+/**
+ * 
+ * @param {string} videoId 
+ * @returns 
+ */
 async function getVideo(videoId){
-    if (!/^[0-9]+$/.test(videoId)){
-        console.error('Invalid videoId');
-        return;
-    }
+  if (!/^[0-9]+$/.test(videoId)){
+    console.error('Invalid videoId');
+    return;
+  }
 
-    const cachedData = getDataFromCache(`video_${videoId}`);
-    if (!!cachedData) return JSON.parse(cachedData);
+  const cachedData = getDataFromCache(`video_${videoId}`);
+  if (!!cachedData) return JSON.parse(cachedData);
 
-    const { data } = await axios.get(`http://techfree-oreumi-api.kro.kr:5000/video/getVideoInfo?video_id=${parseInt(videoId, 10)}`);
-    if (!!data) insertDataInCache(`video_${videoId}`, JSON.stringify(data));
+  const { data } = await axios.get(`${baseUrl}/video/getVideoInfo?video_id=${parseInt(videoId, 10)}`);
+  if (!!data) insertDataInCache(`video_${videoId}`, JSON.stringify(data));
 
-    return data;
+  return data;
 }
 
+/**
+ * 
+ * @param {string|number} channelId 
+ * @returns 
+ */
 async function getChannel(channelId){
-    if (!/^[0-9]+$/.test(channelId)){
-        console.error('Invalid channelId');
-        return;
-    }
+  if (!/^[0-9]+$/.test(channelId)){
+    console.error('Invalid channelId');
+    return;
+  }
 
-    const cachedData = getDataFromCache(`channel_${channelId}`);
-    if (!!cachedData) return JSON.parse(cachedData);
+  const cachedData = getDataFromCache(`channel_${channelId}`);
+  if (!!cachedData) return JSON.parse(cachedData);
 
-    const { data } = await axios.get(`http://techfree-oreumi-api.kro.kr:5000/channel/getChannelInfo?id=${parseInt(channelId, 10)}`);
-    if (!!data) insertDataInCache(`channel_${channelId}`, JSON.stringify(data));
+  const { data } = await axios.get(`${baseUrl}/channel/getChannelInfo?id=${parseInt(channelId, 10)}`);
+  if (!!data) insertDataInCache(`channel_${channelId}`, JSON.stringify(data));
 
-    return data;
+  return data;
 }
 
 async function getVideos(){
-    const { data } = await axios.get('http://techfree-oreumi-api.kro.kr:5000/video/getVideoList');
+  const { data } = await axios.get(`${baseUrl}/video/getVideoList`);
 
-    return data;
+  return data;
 }
 
+/**
+ * 
+ * @param {string} chId 
+ * @returns 
+ */
 async function getChannelVideos(chId){
-    if (!/^[0-9]+$/.test(chId)){
-        console.error('Invalid channelId');
-        return;
-    }
+  if (!/^[0-9]+$/.test(chId)){
+    console.error('Invalid channelId');
+    return;
+  }
 
-    const { data } = await axios.get(`http://techfree-oreumi-api.kro.kr:5000/video/getChannelVideoList?channel_id=${parseInt(chId, 10)}`);
+  const { data } = await axios.get(`${baseUrl}/video/getChannelVideoList?channel_id=${parseInt(chId, 10)}`);
 
-    return data;
+  return data;
 }
 
 export { getVideo, getChannel, getVideos, getChannelVideos, };
