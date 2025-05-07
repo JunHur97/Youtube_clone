be44a3def5751ed98e8023a225bf5766e8e4f2fd
renetdevp <renetdevp@gmail.com>
2025-05-07 09:34:54 +0900

    MOD: Change indent, Add JS function params description

--- static/js/subscribe.js
+++ static/js/subscribe.js
@@ -1,56 +1,71 @@
 import { getDataFromCache, insertDataInCache } from './localCache.js';
 
+/**
+ * 
+ * @param {string} chId 
+ * @returns 
+ */
 function subscribe(chId){
-    const subList = JSON.parse(getDataFromCache('subList'));
-    if (subList.includes(chId)) return unsubscribe(chId);
+  const subList = JSON.parse(getDataFromCache('subList'));
+  if (subList.includes(chId)) return unsubscribe(chId);
 
-    subList.push(chId);
-    insertDataInCache('subList', JSON.stringify(subList));
+  subList.push(chId);
+  insertDataInCache('subList', JSON.stringify(subList));
 
-    const subEvent = new CustomEvent('subscribe', { detail: { chId, } });
-    $('section.subscription')[0].dispatchEvent(subEvent);
+  const subEvent = new CustomEvent('subscribe', { detail: { chId, } });
+  $('section.subscription')[0].dispatchEvent(subEvent);
 }
 
+/**
+ * 
+ * @param {string} chId 
+ * @returns 
+ */
 function unsubscribe(chId){
-    let subList = JSON.parse(getDataFromCache('subList'));
-    if (!subList.includes(chId)) return subscribe(chId);
+  let subList = JSON.parse(getDataFromCache('subList'));
+  if (!subList.includes(chId)) return subscribe(chId);
 
-    subList = subList.filter(v => v !== chId);
-    insertDataInCache('subList', JSON.stringify(subList));
+  subList = subList.filter(v => v !== chId);
+  insertDataInCache('subList', JSON.stringify(subList));
 
-    const unsubEvent = new CustomEvent('unsubscribe', { detail: { chId, } });
-    $('section.subscription')[0].dispatchEvent(unsubEvent);
+  const unsubEvent = new CustomEvent('unsubscribe', { detail: { chId, } });
+  $('section.subscription')[0].dispatchEvent(unsubEvent);
 }
 
+/**
+ * 
+ * @param {Event} e 
+ * @returns 
+ */
 function onSubBtnClick(e){
-    const subBtn = e.target;
-    const chId = parseInt(subBtn.getAttribute('chId'), 10);
+  const subBtn = e.target;
+  const chId = parseInt(subBtn.getAttribute('chId'), 10);
 
-    if (!chId) return;
+  if (!chId) return;
 
-    if (subBtn.hasAttribute('subscribed')){
-        unsubscribe(chId);
-        subBtn.innerText = 'SUBSCRIBES';
-        subBtn.removeAttribute('subscribed');
-    }else {
-        subscribe(chId);
-        subBtn.innerText = 'SUBSCRIBED';
-        subBtn.setAttribute('subscribed', '');
-    }
+  if (subBtn.hasAttribute('subscribed')){
+    unsubscribe(chId);
+    subBtn.innerText = 'SUBSCRIBES';
+    subBtn.removeAttribute('subscribed');
+  }else {
+    subscribe(chId);
+    subBtn.innerText = 'SUBSCRIBED';
+    subBtn.setAttribute('subscribed', '');
+  }
 }
 
 // sessionStorage.subList init
 $(document).ready(() => {
-    if (!sessionStorage.getItem('subList')){
-        // sessionStorage.clear();
+  if (!sessionStorage.getItem('subList')){
+    // sessionStorage.clear();
 
-        const subList = [];
+    const subList = [];
 
-        subList.push(1);
-        subList.push(3);
+    subList.push(1);
+    subList.push(3);
 
-        insertDataInCache('subList', JSON.stringify(subList));
-    }
+    insertDataInCache('subList', JSON.stringify(subList));
+  }
 });
 
 export { onSubBtnClick, };
--- static/js/videoPage.js
+++ static/js/videoPage.js
@@ -2,127 +2,141 @@ import { getVideo, getChannel, getVideos } from './modules/axiosReq.js';
 import { onSubBtnClick } from './subscribe.js';
 import { getDataFromCache } from './localCache.js';
 
+/**
+ * 
+ * @param {string} urlSearch 
+ * @returns 
+ */
 function getVideoId(urlSearch){
-    const hos = urlSearch.slice(1).split('&').filter(v => v.startsWith('video_id'));
-    return hos[0].split('=')[1];
+  const hos = urlSearch.slice(1).split('&').filter(v => v.startsWith('video_id'));
+  return hos[0].split('=')[1];
 }
 
 function setVideoInfo(videoInfo){
-    $('.videoPlayer')[0].src = `https://storage.googleapis.com/youtube-clone-video/${videoInfo.id}.mp4`;
-    $('.videoMetadata .title')[0].textContent = videoInfo.title;
-    $('.videoMetadata .uploadDate')[0].textContent = moment(videoInfo.created_dt).format('MMM D, YYYY');
-    $('.videoMetadata .views')[0].textContent = `${nFormatter(videoInfo.views, 1)} views`;
-    $('.videoMetadata .dislikes p')[0].innerText = nFormatter(videoInfo.dislikes, 1);
-    $('.videoMetadata .likes p')[0].innerText = nFormatter(videoInfo.likes, 1);
-    $('.videoDescription .videoDescText')[0].innerText = videoInfo.description;
+  $('.videoPlayer')[0].src = `https://storage.googleapis.com/youtube-clone-video/${videoInfo.id}.mp4`;
+  $('.videoMetadata .title')[0].textContent = videoInfo.title;
+  $('.videoMetadata .uploadDate')[0].textContent = moment(videoInfo.created_dt).format('MMM D, YYYY');
+  $('.videoMetadata .views')[0].textContent = `${nFormatter(videoInfo.views, 1)} views`;
+  $('.videoMetadata .dislikes p')[0].innerText = nFormatter(videoInfo.dislikes, 1);
+  $('.videoMetadata .likes p')[0].innerText = nFormatter(videoInfo.likes, 1);
+  $('.videoDescription .videoDescText')[0].innerText = videoInfo.description;
 };
 
 function setChannelInfo(channelInfo){
-    $('.videoUploader img')[0].src = channelInfo.channel_profile;
-    $('.videoUploader > a')[0].href = `/channels?ch_id=${channelInfo.id}`;
-    $('.videoUploader > button')[0].setAttribute('chId', channelInfo.id);
-    $('.uploaderInfo .uploaderName')[0].innerText = channelInfo.channel_name;
-    $('.uploaderInfo .uploaderName')[0].href = `/channels?ch_id=${channelInfo.id}`;
-    $('.uploaderInfo .uploaderSubscribers')[0].innerText = `${nFormatter(channelInfo.subscribers, 1)} subscribers`;
+  $('.videoUploader img')[0].src = channelInfo.channel_profile;
+  $('.videoUploader > a')[0].href = `/channels?ch_id=${channelInfo.id}`;
+  $('.videoUploader > button')[0].setAttribute('chId', channelInfo.id);
+  $('.uploaderInfo .uploaderName')[0].innerText = channelInfo.channel_name;
+  $('.uploaderInfo .uploaderName')[0].href = `/channels?ch_id=${channelInfo.id}`;
+  $('.uploaderInfo .uploaderSubscribers')[0].innerText = `${nFormatter(channelInfo.subscribers, 1)} subscribers`;
 
-    setSubBtn(channelInfo.id);
+  setSubBtn(channelInfo.id);
 };
 
+/**
+ * 
+ * @param {string} title 
+ */
 function setDocumentTitle(title){
-    document.title = title;
+  document.title = title;
 }
 
 function setVideoKeyControl(){
-    $(document).keypress((e) => {
-        e.preventDefault();
+  $(document).keypress((e) => {
+    e.preventDefault();
 
-        if (e.key === ' '){
-            const video = $('.videoMain > .videoPlayer')[0];
+    if (e.key === ' '){
+      const video = $('.videoMain > .videoPlayer')[0];
 
-            video.paused ? video.play() : video.pause();
-        }
-    });
+      video.paused ? video.play() : video.pause();
+    }
+  });
 }
 
+/**
+ * 
+ * @param {string} chId 
+ * @returns 
+ */
 function setSubBtn(chId){
-    const subList = JSON.parse(getDataFromCache('subList'));
+  const subList = JSON.parse(getDataFromCache('subList'));
 
-    if (!subList) return;
+  if (!subList) return;
 
-    if (subList.includes(chId)){
-        const uploaderBtn = $('.videoUploader > button')[0];
+  if (subList.includes(chId)){
+    const uploaderBtn = $('.videoUploader > button')[0];
 
-        uploaderBtn.setAttribute('subscribed', '');
-        uploaderBtn.innerText = 'SUBSCRIBED';
-    }
+    uploaderBtn.setAttribute('subscribed', '');
+    uploaderBtn.innerText = 'SUBSCRIBED';
+  }
 }
 
 function setSubBtnOnClick(){
-    const subBtn = $('.videoUploader > button')[0];
+  const subBtn = $('.videoUploader > button')[0];
 
-    // subscribe.js/onSubBtnClick()
-    subBtn.addEventListener('click', onSubBtnClick);
+  // subscribe.js/onSubBtnClick()
+  subBtn.addEventListener('click', onSubBtnClick);
 }
 
 async function setVideoMain(){
-    const videoId = getVideoId(window.location.search);
+  const videoId = getVideoId(window.location.search);
 
-    try {
-        const res = await getVideo(videoId);
-        const channelRes = await getChannel(res.channel_id);
+  try {
+    const res = await getVideo(videoId);
+    const channelRes = await getChannel(res.channel_id);
 
-        setVideoInfo(res);
-        setChannelInfo(channelRes);
-        setDocumentTitle(res.title);
-        setVideoKeyControl();
-        setSubBtnOnClick();
-    }catch (e){
-        axiosErrorHandler(e);
-    }
+    setVideoInfo(res);
+    setChannelInfo(channelRes);
+    setDocumentTitle(res.title);
+    setVideoKeyControl();
+    setSubBtnOnClick();
+  }catch (e){
+    axiosErrorHandler(e);
+  }
 }
 
 async function setVideoNav(){
-    try {
-        const res = await getVideos();
+  try {
+    const res = await getVideos();
 
-        res.forEach(async (v) => {
-            if (v.id === parseInt(getVideoId(window.location.search), 10)) return;
+    res.forEach(async (v) => {
+      if (v.id === parseInt(getVideoId(window.location.search), 10)) return;
 
-            const chRes = await getChannel(v.channel_id);
-            const comment = `
-            <div class="rVideo">
-                <a href="/videos?video_id=${v.id}">
-                    <img src="${v.thumbnail}">
-                </a>
-                <div class="rVideoInfo">
-                    <a class="rVideoTitle" href="/videos?video_id=${v.id}">${v.title}</a>
-                    <a class="rVideoUploader" href="/channels?ch_id=${v.channel_id}">${chRes.channel_name}</a>
-                    <div class="rVideoBottom">
-                        <p>${nFormatter(v.views, 1)} views</p>
-                        <p>${moment(v.created_dt).fromNow()}</p>
-                    </div>
-                </div>
-            </div>`;
+      const chRes = await getChannel(v.channel_id);
+      const comment = `
+      <div class="rVideo">
+        <a href="/videos?video_id=${v.id}">
+          <img src="${v.thumbnail}">
+        </a>
+        <div class="rVideoInfo">
+          <a class="rVideoTitle" href="/videos?video_id=${v.id}">${v.title}</a>
+          <a class="rVideoUploader" href="/channels?ch_id=${v.channel_id}">${chRes.channel_name}</a>
+          <div class="rVideoBottom">
+            <p>${nFormatter(v.views, 1)} views</p>
+            <p>${moment(v.created_dt).fromNow()}</p>
+          </div>
+        </div>
+      </div>`;
 
-            $('.videoNav')[0].insertAdjacentHTML('beforeend', comment);
-        });
-    }catch (e){
-        axiosErrorHandler(e);
-    }
+      $('.videoNav')[0].insertAdjacentHTML('beforeend', comment);
+    });
+  }catch (e){
+    axiosErrorHandler(e);
+  }
 }
 
 function setVideoPageTopbar(){
-    window.addEventListener('load', (e) => {
-        $('.navBar')[0].style.display = 'none';
+  window.addEventListener('load', (e) => {
+    $('.navBar')[0].style.display = 'none';
 
-        $('button[aria-label="Menu"]').click((e) => {
-            $('.navBar').animate({ width: 'toggle' }, 200);
-        })
+    $('button[aria-label="Menu"]').click((e) => {
+      $('.navBar').animate({ width: 'toggle' }, 200);
     });
+  });
 }
 
 $(document).ready(async () => {
-    setVideoPageTopbar();
-    await setVideoMain();
-    await setVideoNav();
+  setVideoPageTopbar();
+  await setVideoMain();
+  await setVideoNav();
 });
